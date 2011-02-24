fdb5c51e97de5118a605f2cd6789100844d3722e
Zach Holman <github.com@zachholman.com>
2011-02-24 01:00:45 -0800

    OS X can recognize if Ellen Page is hot or not

---
+++ 5-facetime/recognize.rb
@@ -0,0 +1,25 @@
+#
+# Modified from: https://gist.github.com/402759
+#
+# Mr. Aimonetti also gave a more expanded version of this during his 2010
+# RubyConf talk, which maybe my subconscious drummed up enough to give me the
+# idea for parts of this segment.
+
+framework 'AppKit'
+
+app = NSApplication.sharedApplication
+
+class AppDelegate
+  def speechRecognizer(sender, didRecognizeCommand:command)
+    puts "command: #{command}"
+    exit!
+  end
+end
+
+recognizer = NSSpeechRecognizer.alloc.init
+recognizer.commands = ["ellen", "page", "ellen page"]
+recognizer.delegate = AppDelegate.new
+recognizer.ListensInForegroundOnly = false
+recognizer.startListening
+
+app.run
